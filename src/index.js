523226ec32d9b462eba155cefcf1287c13d9cb9a
Sky <sky@javadesign.club>
2021-02-26 15:43:48 +0800

    [Handling arrays] 对于数组而言,不能直接改变现有数组,需要返回数组 新副本 ,而后添加新的元素到数组中;

--- src/index.js
+++ src/index.js
@@ -9,14 +9,23 @@ const App = () => {
         left: 0,
         right: 0
     })
-    const handleLeftClick = () => setClicks({
-        ...clicks,
-        left: clicks.left + 1
-    })
-    const handleRightClick = () => setClicks({
-        ...clicks,
-        right: clicks.right + 1
-    })
+
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setClicks({
+            ...clicks,
+            left: clicks.left + 1
+        })
+        setAll(allClicks.concat('L'))
+    }
+    const handleRightClick = () => {
+        setClicks({
+            ...clicks,
+            right: clicks.right + 1
+        })
+        setAll(allClicks.concat('R'))
+    }
 
     return (
         <div>
@@ -25,6 +34,8 @@ const App = () => {
 
             <button onClick={handleRightClick}>right</button>
             {clicks.right}
+
+            <p>{allClicks.join(' ')}</p>
         </div>
     )
 }
