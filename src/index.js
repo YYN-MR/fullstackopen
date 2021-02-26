56da86f0c4d208acc23c40e1f80b46732af4998f
Sky <sky@javadesign.club>
2021-02-26 16:15:03 +0800

    [Function that returns a function: step01] 每个按钮都有自己单独的时间处理程序; 返回函数的函数可用于定义可以使用参数自定义的通用函数。 可以将创建事件处理程序的 hello 函数视为一个生成用户的定制事件处理的工厂。

--- src/index.js
+++ src/index.js
@@ -11,17 +11,18 @@ import reportWebVitals from './reportWebVitals';
  */
 const App = () => {
     const [value, setValue] = useState(10)
-    const hello = () => {
-        return () => {
-            console.log('hello world')
-            setValue(value + 1)
-        }
+    //返回函数的函数可用于定义可以使用参数自定义的通用函数。 可以将创建事件处理程序的 hello 函数视为一个生成用户的定制事件处理的工厂。
+    const hello = (who) => () => {
+        console.log('hello ', who)
+        setValue(value + 1)
     }
     return (
         <div>
             {value}
             {/*函数调用的返回值是函数,此时函数调用会起作用*/}
-            <button onClick={hello()}>button</button>
+            <button onClick={hello('Sky')}>button</button>
+            <button onClick={hello('function')}>button</button>
+            <button onClick={hello('react')}>button</button>
         </div>
     )
 }
