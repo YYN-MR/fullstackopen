e39d781ce74a41353b4a7b88eed64560e921f6ec
Sky <sky@javadesign.club>
2021-02-26 16:35:45 +0800

    [Function that returns a function: step02] 写一个计数器;

--- src/index.js
+++ src/index.js
@@ -11,18 +11,16 @@ import reportWebVitals from './reportWebVitals';
  */
 const App = () => {
     const [value, setValue] = useState(10)
-    //返回函数的函数可用于定义可以使用参数自定义的通用函数。 可以将创建事件处理程序的 hello 函数视为一个生成用户的定制事件处理的工厂。
-    const hello = (who) => () => {
-        console.log('hello ', who)
-        setValue(value + 1)
+    const setToValue = (newValue) => () => {
+        setValue(newValue)
     }
     return (
         <div>
             {value}
             {/*函数调用的返回值是函数,此时函数调用会起作用*/}
-            <button onClick={hello('Sky')}>button</button>
-            <button onClick={hello('function')}>button</button>
-            <button onClick={hello('react')}>button</button>
+            <button onClick={setToValue(1000)}>thousand</button>
+            <button onClick={setToValue(0)}>reset</button>
+            <button onClick={setToValue(value + 1)}>increment</button>
         </div>
     )
 }
