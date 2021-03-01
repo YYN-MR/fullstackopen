66ffc2dd765abd9a32d2632b20e49663edba4cc7
Sky <sky@javadesign.club>
2021-03-01 13:45:59 +0800

    [Rendering collections] 从渲染集合到模块学习

--- src/index.js
+++ src/index.js
@@ -3,28 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const Display = props => <div>{props.value}</div>
-
-const Button = ({handleClick, text}) => (
-    <button onClick={handleClick}>{text}</button>
-)
+/**
+ * 留言板内容
+ * @param note
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Note = ({note}) => {
+    return (
+        <li>
+            {note.content}
+        </li>
+    )
+}
 /**
  * App 应用启动组件
  * @returns {JSX.Element}
  * @constructor
  */
 const App = () => {
-    const [value, setValue] = useState(10)
-    const setToValue = (newValue) => {
-        setValue(newValue)
-    }
+    const notes = [
+        {
+            id: 1,
+            content: 'HTML is easy',
+            date: '2019-05-30T17:30:31.098Z',
+            important: true
+        },
+        {
+            id: 2,
+            content: 'Browser can execute only JavaScript',
+            date: '2019-05-30T18:39:34.091Z',
+            important: false
+        },
+        {
+            id: 3,
+            content: 'GET and POST are the most important methods of HTTP protocol',
+            date: '2019-05-30T19:20:14.298Z',
+            important: true
+        }
+    ]
 
     return (
         <div>
-            <Display value={value}/>
-            <Button handleClick={() => setToValue(1000)} text='thousand'/>
-            <Button handleClick={() => setToValue(0)} text='reset'/>
-            <Button handleClick={() => setToValue(value + 1)} text='increment'/>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note => (
+                    <Note key={note.id} note={note}>
+                    </Note>
+                ))}
+            </ul>
         </div>
     )
 }
